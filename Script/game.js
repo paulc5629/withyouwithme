55fe1d8f46263c93db130d60c7e94500b3e5493e
Paul Peter Callaghan <calgahan@live.co.uk>
2025-04-07 15:38:35 +0100

    Commit to start new project

--- Script/game.js
+++ Script/game.js
@@ -13,20 +13,33 @@ window.onload = function() {
 } 
 
 class Game{
-  startGame = function(render) {
-    
+
+  startGame(render){
+    /*
+    This was added but no longer required to new specification
     const playerCount = document.getElementById("player-amount").value;
       if (playerCount<=6 && playerCount>=1){
         for (let i = 0; i < playerCount; i++) {
           window["player"+(i+1)] = new Player("player"+(i+1), 3);
           //https://www.codecademy.com/forum_questions/51068e93f73ad4947a005629 help for creating the player objects
         }
+        //move to new screen to revel first card
+        let render = new Render();
+        
+        render.playScreen();
+        console.log("Game Started");
       }else {
         alert("Number of players must be between 1 and 6.");
-      }
-    //move to new screen to revel first card
-    render = new Render();
-    render.playScreen();
-    console.log("Game Started");
+      }*/
+        let game = new Game();
+      document.getElementById("forfit").onclick = game.forfit();
+      //render = new Render();
+      console.log("Game Started");
   }
+
+  forfit(render){
+    render = new Render();
+    render.forfitScreen();
+  }
+
 }
--- Script/render.js
+++ Script/render.js
@@ -9,8 +9,17 @@ export class Render{
         // Clear previous content
         this.container.innerHTML = '';
         // Add new content
-        this.container.innerHTML = `<h1>Welcome to Revel</h1>`;
-        this.container.innerHTML += `<button type="button">Completed</button>`;
-        return this.container.innerHTML += `<button type="button">Forfit</button>`;
+        this.container.innerHTML = `<h3>Welcome to Revel</h3>`;
+        this.container.innerHTML += `<button >Completed</button>`;
+        this.container.innerHTML += `<button id="forfit">Forfit</button>`;
+        
+        return this.container.innerHTML;
+    }
+
+    forfitScreen = function() {
+        // Add new content
+        this.container.innerHTML = `<h3>Player has forfeited</h3>`;
+        this.container.innerHTML += `<button >Back to Game</button>`;
+        return this.container.innerHTML;
     }
 }
